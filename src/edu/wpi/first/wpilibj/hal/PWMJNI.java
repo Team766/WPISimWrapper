3bad9bf249aef626e183315c8bb6c153e571397b
Blevenson <blevenson68@gmail.com>
2016-06-12 10:59:51 -0700

    Prevent multiple PWM ports being instantiated

--- src/edu/wpi/first/wpilibj/hal/PWMJNI.java
+++ src/edu/wpi/first/wpilibj/hal/PWMJNI.java
@@ -10,63 +10,73 @@ package edu.wpi.first.wpilibj.hal;
 import com.team766.simulator.Ports;
 import com.team766.simulator.VRConnector;
 
+import edu.wpi.first.wpilibj.SensorBase;
+
 public class PWMJNI extends DIOJNI {
-  public static boolean allocatePWMChannel(long digital_port_pointer){
-	  System.out.println("PWMJNI: allocatePWMChannel()");
-	  return true;
-  }
+	private static boolean[] usedPorts = new boolean[SensorBase.kPwmChannels];
 
-  public static void freePWMChannel(long digital_port_pointer){
-	  System.out.println("PWMJNI: freePWMChannel()");
-  }
+	public static boolean allocatePWMChannel(long digital_port_pointer) {
+		System.out.println("PWMJNI: allocatePWMChannel()");
+		if (!usedPorts[(int) digital_port_pointer]) {
+			usedPorts[(int) digital_port_pointer] = true;
+			return true;
+		}
+		return false;
+	}
 
-  public static void setPWM(long digital_port_pointer, double value){
-//	 System.out.println("PWMJNI: setPWM()");
-	  
-	  //Cap values between [-1, 1]
-//	  value = Math.abs(value) > 1? 1.0 * (Math.abs(value)/value) : value ;
-	  
-	  if(digital_port_pointer == Ports.PWM_Left_Drive){
-		  VRConnector.getInstance().putCommandFloat(VRConnector.LEFT_MOTOR, (float)value);
-//		  System.out.println("Left Drive: " + value);
-	  }
-	  else if(digital_port_pointer == Ports.PWM_Right_Drive){
-		  VRConnector.getInstance().putCommandFloat(VRConnector.RIGHT_MOTOR, (float)value);
-//		  System.out.println("Right Drive: " + value);
-	  }
-  }
+	public static void freePWMChannel(long digital_port_pointer) {
+		System.out.println("PWMJNI: freePWMChannel()");
+	}
 
-  public static short getPWM(long digital_port_pointer){
-	  System.out.println("PWMJNI: getPWM()");
-	  return 1;
-  }
+	public static void setPWM(long digital_port_pointer, double value) {
+		// System.out.println("PWMJNI: setPWM()");
 
-  public static void latchPWMZero(long digital_port_pointer){
-	  System.out.println("PWMJNI: latchPWMZero()");
-  }
+		// Cap values between [-1, 1]
+		// value = Math.abs(value) > 1? 1.0 * (Math.abs(value)/value) : value ;
 
-  public static void setPWMPeriodScale(long digital_port_pointer, int squelchMask){
-	  System.out.println("PWMJNI: setPWMPeriodScale()");
-  }
+		if (digital_port_pointer == Ports.PWM_Left_Drive) {
+			VRConnector.getInstance().putCommandFloat(VRConnector.LEFT_MOTOR,
+					(float) value);
+			// System.out.println("Left Drive: " + value);
+		} else if (digital_port_pointer == Ports.PWM_Right_Drive) {
+			VRConnector.getInstance().putCommandFloat(VRConnector.RIGHT_MOTOR,
+					(float) value);
+			// System.out.println("Right Drive: " + value);
+		}
+	}
 
-  public static long allocatePWM(){
-	  System.out.println("PWMJNI: allocatePWM()");
-	  return 1;
-  }
+	public static short getPWM(long digital_port_pointer) {
+		System.out.println("PWMJNI: getPWM()");
+		return 1;
+	}
 
-  public static void freePWM(long pwmGenerator){
-	  System.out.println("PWMJNI: freePWM()");
-  }
-  
-  public static void setPWMRate(double rate){
-	  System.out.println("PWMJNI: setPWMRate()");
-  }
+	public static void latchPWMZero(long digital_port_pointer) {
+		System.out.println("PWMJNI: latchPWMZero()");
+	}
 
-  public static void setPWMDutyCycle(long pwmGenerator, double dutyCycle){
-	  System.out.println("PWMJNI: setPWMDutyCycle()");
-  }
+	public static void setPWMPeriodScale(long digital_port_pointer,
+			int squelchMask) {
+		System.out.println("PWMJNI: setPWMPeriodScale()");
+	}
 
-  public static void setPWMOutputChannel(long pwmGenerator, int pin){
-	  System.out.println("PWMJNI: setPWMOutputChannel()");
-  }
+	public static long allocatePWM() {
+		System.out.println("PWMJNI: allocatePWM()");
+		return 1;
+	}
+
+	public static void freePWM(long pwmGenerator) {
+		System.out.println("PWMJNI: freePWM()");
+	}
+
+	public static void setPWMRate(double rate) {
+		System.out.println("PWMJNI: setPWMRate()");
+	}
+
+	public static void setPWMDutyCycle(long pwmGenerator, double dutyCycle) {
+		System.out.println("PWMJNI: setPWMDutyCycle()");
+	}
+
+	public static void setPWMOutputChannel(long pwmGenerator, int pin) {
+		System.out.println("PWMJNI: setPWMOutputChannel()");
+	}
 }
